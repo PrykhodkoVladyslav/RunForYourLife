48965d7b1f573fdbe4df02693e6a9f44ca95a241
Владислав Приходько <3885vlad@gmail.com>
2025-05-28 22:49:29 +0300

    Add main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button fallbackButton;
+
+    private UiInputAction _uiInputAction;
+    private InputAction _navigateInputAction;
+
+    private void Awake()
+    {
+        _uiInputAction = new UiInputAction();
+    }
+
+    private void OnEnable()
+    {
+        _navigateInputAction = _uiInputAction.UI.Navigate;
+        _navigateInputAction.Enable();
+
+        _navigateInputAction.performed += OnNavigatePositionChanged;
+        _navigateInputAction.canceled += OnNavigatePositionChanged;
+    }
+
+    private void OnDisable()
+    {
+        _navigateInputAction.performed -= OnNavigatePositionChanged;
+        _navigateInputAction.canceled -= OnNavigatePositionChanged;
+
+        _navigateInputAction.Disable();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Field");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    private void OnNavigatePositionChanged(InputAction.CallbackContext obj)
+    {
+        if (!EventSystem.current.currentSelectedGameObject)
+        {
+            EventSystem.current.SetSelectedGameObject(fallbackButton.gameObject);
+        }
+    }
+}
